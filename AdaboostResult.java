7cfc61dfc7d3d553542062e9da1e184eaa4dd937
EverKnows <30998371+EverKnows@users.noreply.github.com>
2020-07-04 16:41:26 +0800

    Add files via upload

---
+++ AdaboostResult.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class AdaboostResult  {
+    private ArrayList<ArrayList<Double>> weakClassifierSet;
+    private ArrayList<Double> classifierWeightSet;
+
+    public ArrayList<ArrayList<Double>> getWeakClassifierSet() {
+        return weakClassifierSet;
+    }
+    public void setWeakClassifierSet(ArrayList<ArrayList<Double>> weakClassifierSet) {
+        this.weakClassifierSet = weakClassifierSet;
+    }
+    public ArrayList<Double> getClassifierWeightSet() {
+        return classifierWeightSet;
+    }
+    public void setClassifierWeightSet(ArrayList<Double> classifierWeightSet) {
+        this.classifierWeightSet = classifierWeightSet;
+    }
+}
